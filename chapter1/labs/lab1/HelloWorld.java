0a319f92763d1333a5d5d0b33a22b3f95668a45f
namung08 <namung08@gmail.com>
2025-04-28 12:09:25 +0900

    :sparkles: feat: lab1 helloworld 코드 작성

--- chapter1/labs/lab1/HelloWorld.java
+++ chapter1/labs/lab1/HelloWorld.java
@@ -1,4 +1,4 @@
-package lab1;
+package chapter1.labs.lab1;
 
 /**
  * 첫 번째 자바 프로그램 - Hello World
@@ -7,10 +7,10 @@ package lab1;
 public class HelloWorld {
     public static void main(String[] args) {
         // TODO: "Hello World"를 출력하는 코드를 작성하세요.
-        
+        System.out.println("Hello World");
         // TODO: 자신의 이름을 출력하는 코드를 작성하세요.
-        
+        System.out.println("제 이름은 이상준 입니다.");
         // TODO: 여러 줄의 메시지를 출력하는 코드를 작성하세요.
-        
+        System.out.println("안\n녕.");
     }
-} 
+}
