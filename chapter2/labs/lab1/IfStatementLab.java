4211688f254c676f3dc9eb1c7817af1a70a4313f
namung08 <namung08@gmail.com>
2025-04-29 10:30:49 +0900

    :sparkles: feat: chapter2 lab1 IfStatementLab 코드 작성

--- chapter2/labs/lab1/IfStatementLab.java
+++ chapter2/labs/lab1/IfStatementLab.java
@@ -8,31 +8,43 @@ import java.util.Scanner;
 public class IfStatementLab {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         // 1. 사용자로부터 숫자를 입력받아 양수, 음수, 또는 0인지 판별하는 프로그램
         System.out.println("숫자를 입력하세요: ");
         int number = scanner.nextInt();
-        
+
         // TODO: 입력받은 숫자가 양수, 음수, 0인지 판별하고 출력하세요.
         // 양수이면 "양수입니다."
         // 음수이면 "음수입니다."
         // 0이면 "0입니다."
-        
-        
+
+        if (number > 0) {
+            System.out.println("양수입니다.");
+        } else if (number < 0) {
+            System.out.println("음수입니다.");
+        }
+
+
         // 2. 두 숫자를 입력받아 큰 수를 출력하는 프로그램
         System.out.println("\n첫 번째 숫자를 입력하세요: ");
         int num1 = scanner.nextInt();
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = scanner.nextInt();
-        
+
         // TODO: 두 숫자 중 큰 수를 출력하세요.
         // 만약 두 수가 같다면 "두 수는 같습니다."라고 출력하세요.
-        
-        
+        if (num1 > num2) {
+            System.out.println(num1);
+        } else if (num1 < num2) {
+            System.out.println(num2);
+        } else {
+            System.out.println("두 수는 같습니다.");
+        }
+
         // 3. 점수를 입력받아 학점을 출력하는 프로그램
         System.out.println("\n시험 점수를 입력하세요 (0-100): ");
         int score = scanner.nextInt();
-        
+
         // TODO: 입력받은 점수에 따라 학점을 출력하세요.
         // 90-100: "A"
         // 80-89: "B"
@@ -40,8 +52,20 @@ public class IfStatementLab {
         // 60-69: "D"
         // 0-59: "F"
         // 범위를 벗어난 점수: "잘못된 점수입니다."
-        
-        
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else if (score >= 0) {
+            System.out.println("F");
+        } else {
+            System.out.println("잘못된 점수입니다.");
+        }
+
         scanner.close();
     }
-} 
+}
