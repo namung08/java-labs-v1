133e05d58835187de22121987ed4788df14ee1fa
namung08 <namung08@gmail.com>
2025-04-29 16:48:08 +0900

    :sparkles: feat: chapter3 lab2,3 ArrayLab, StringLab 코드 작성

--- chapter3/labs/lab2/ArrayLab.java
+++ chapter3/labs/lab2/ArrayLab.java
@@ -8,42 +8,82 @@ import java.util.Scanner;
 public class ArrayLab {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         // 1. 정수형 배열 활용
         System.out.println("===== 정수형 배열 활용 =====");
-        
+
         // TODO: 5개의 정수를 입력받아 배열에 저장하고, 합계와 평균을 계산하여 출력하세요.
         // 힌트: Scanner를 이용하여 사용자로부터 입력을 받으세요.
-        
-        
+
+        int[] array = new int[5];
+
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i + 1) + "번째 숫자를 입력해 주세요.");
+            int num = scanner.nextInt();
+            array[i] = num;
+            sum += num;
+        }
+
+        int avg = sum / array.length;
+        System.out.println("입력 받은 수의 합은: " + sum);
+        System.out.println("입력 받은 수의 평균은: " + avg);
+
         // 2. 문자열 배열 활용
         System.out.println("\n===== 문자열 배열 활용 =====");
-        
+
         // TODO: 5개의 이름을 입력받아 배열에 저장하고, 특정 문자(예: 'A')로 시작하는 이름을 검색하여 출력하세요.
         // 힌트: String 클래스의 startsWith() 또는 charAt() 메소드를 활용하세요.
-        
-        
+
+        String[] names = new String[5];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i + 1) + "번째 이름을 입력해 주세요.");
+            String name = scanner.next();
+            names[i] = name;
+        }
+
+        StringBuilder startAName = new StringBuilder();
+        for (String name : names) {
+            if (name.startsWith("A") || name.startsWith("a")) {
+                if (startAName.length() > 0) {
+                    startAName.append(", ");
+                }
+                startAName.append(name);
+            }
+        }
+
+        System.out.println("'A'로 시작되는 이름은: " + startAName + " 입니다.");
+
+
         // 3. 배열 정렬
         System.out.println("\n===== 배열 정렬 =====");
-        
+
         // TODO: 사용자로부터 5개의 정수를 입력받아 배열에 저장한 후, 오름차순으로 정렬하여 출력하세요.
         // 힌트: 이중 반복문을 사용하여 버블 정렬 알고리즘을 구현하거나, Arrays.sort()를 활용하세요.
-        
-        
+        int[] array2 = new int[5];
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println((i + 1) + "번째 숫자를 입력해 주세요.");
+            array2[i] = scanner.nextInt();
+        }
+
+
+
         // 4. 2차원 배열을 이용한 행렬 덧셈
         System.out.println("\n===== 2차원 배열을 이용한 행렬 덧셈 =====");
-        
+
         // TODO: 두 개의 2x3 행렬을 입력받아 행렬 덧셈을 수행하고 결과를 출력하세요.
         // 힌트: 중첩 반복문을 사용하세요.
-        
-        
+
+
         // 5. 배열을 이용한 간단한 데이터 분석
         System.out.println("\n===== 배열을 이용한 데이터 분석 =====");
-        
+
         // TODO: 10개의 정수를 입력받아 배열에 저장한 후, 최댓값, 최솟값, 중앙값(또는 평균)을 계산하여 출력하세요.
         // 힌트: 정렬을 활용하면 중앙값을 쉽게 찾을 수 있습니다.
-        
-        
+
+
         scanner.close();
     }
-} 
+}
--- chapter3/labs/lab3/StringLab.java
+++ chapter3/labs/lab3/StringLab.java
@@ -8,47 +8,73 @@ import java.util.Scanner;
 public class StringLab {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         // 1. 단어 수 계산하기
         System.out.println("===== 단어 수 계산하기 =====");
         System.out.println("문장을 입력하세요: ");
         String sentence = scanner.nextLine();
-        
+
         // TODO: 입력받은 문장에서 단어 수를 계산하여 출력하세요.
         // 힌트: split() 메소드를 사용하여 문자열을 공백으로 분리하세요.
-        
-        
+        String[] strings = sentence.split(" ");
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+
         // 2. 문자열 뒤집기
         System.out.println("\n===== 문자열 뒤집기 =====");
         System.out.println("문자열을 입력하세요: ");
         String original = scanner.nextLine();
-        
+
         // TODO: 입력받은 문자열을 뒤집어서 출력하세요.
         // 힌트: 문자 배열로 변환하거나, StringBuilder의 reverse() 메소드를 활용하세요.
-        
-        
+
+        System.out.println(new StringBuilder(original).reverse());
+
+
         // 3. 특정 문자 등장 횟수 계산하기
         System.out.println("\n===== 특정 문자 등장 횟수 계산하기 =====");
         System.out.println("문자열을 입력하세요: ");
         String text = scanner.nextLine();
         System.out.println("찾을 문자를 입력하세요: ");
         char target = scanner.next().charAt(0);
-        
+
         // TODO: 입력받은 문자열에서 특정 문자의 등장 횟수를 계산하여 출력하세요.
         // 힌트: 문자열을 순회하면서 각 문자와 대상 문자를 비교하세요.
-        
-        
+
+        int count = 0;
+
+        System.out.println(text.length());
+
+        char[] chars = text.toCharArray();
+
+        for (char ch : chars) {
+            if (ch == target) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
         // 4. 회문(Palindrome) 검사
         System.out.println("\n===== 회문(Palindrome) 검사 =====");
         scanner.nextLine(); // 버퍼 비우기
         System.out.println("문자열을 입력하세요: ");
         String palindrome = scanner.nextLine();
-        
+
         // TODO: 입력받은 문자열이 회문인지 검사하여 결과를 출력하세요.
         // 회문은 앞으로 읽으나 뒤로 읽으나 같은 단어나 문장을 의미합니다 (예: "level", "civic").
         // 힌트: 문자열을 뒤집은 후 원래 문자열과 비교하세요.
 
-        
+        String reversedPalindrome = String.valueOf(new StringBuilder(palindrome).reverse());
+
+        if (palindrome.equals(reversedPalindrome)) {
+            System.out.println("입력하신 문자열은 회문입니다.");
+        } else {
+            System.out.println("입력하신 문자열은 회문이 아닙니다.");
+        }
+
         scanner.close();
     }
-} 
+}
