5f560ff079b7c986988a795838d9452d948be16a
namung08 <namung08@gmail.com>
2025-04-30 16:11:57 +0900

    :sparkles: feat: chapter4 lab2 BookTest 코드 작성

--- chapter4/labs/lab2/Book.java
+++ chapter4/labs/lab2/Book.java
@@ -1,36 +1,53 @@
 package chapter4.labs.lab2;
 
+import lombok.Setter;
+
 /**
  * Lab 2: 생성자 활용하기
- * 
+ *
  * Book 클래스 정의하기
  */
+@Setter
 public class Book {
-    // TODO: 제목, 저자, 가격, 출판연도를 저장할 수 있는 필드를 정의하세요.
-    
-    
-    
-    // TODO: 기본 생성자를 정의하세요.
-    
-    
-    
-    // TODO: 제목만 인자로 받는 생성자를 정의하세요. (this() 사용)
-    
-    
-    
-    // TODO: 제목과 저자를 인자로 받는 생성자를 정의하세요. (this() 사용)
-    
-    
-    
-    // TODO: 제목, 저자, 가격을 인자로 받는 생성자를 정의하세요. (this() 사용)
-    
-    
-    
-    // TODO: 모든 필드를 인자로 받는 생성자를 정의하세요.
-    
-    
-    
-    // TODO: 책의 정보를 출력하는 메소드를 정의하세요.
-    
-    
-} 
+  // TODO: 제목, 저자, 가격, 출판연도를 저장할 수 있는 필드를 정의하세요.
+  private String title;
+  private String author;
+  private int price;
+  private int year;
+
+  // TODO: 기본 생성자를 정의하세요.
+  public Book() {
+  }
+
+  // TODO: 제목만 인자로 받는 생성자를 정의하세요. (this() 사용)
+  public Book(String title) {
+    this.title = title;
+  }
+
+  // TODO: 제목과 저자를 인자로 받는 생성자를 정의하세요. (this() 사용)
+  public Book(String title, String author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  // TODO: 제목, 저자, 가격을 인자로 받는 생성자를 정의하세요. (this() 사용)
+  public Book(String title, String author, int price) {
+    this.title = title;
+    this.author = author;
+    this.price = price;
+  }
+
+  // TODO: 모든 필드를 인자로 받는 생성자를 정의하세요.
+  public Book(String title, String author, int price, int year) {
+    this.title = title;
+    this.author = author;
+    this.price = price;
+    this.year = year;
+  }
+
+  // TODO: 책의 정보를 출력하는 메소드를 정의하세요.
+  public void print() {
+    System.out.println("Title: " + title + "\nAuthor: " + author + "\nPrice: " + price + "\nYear: " + year);
+  }
+
+}
--- chapter4/labs/lab2/BookTest.java
+++ chapter4/labs/lab2/BookTest.java
@@ -2,35 +2,36 @@ package chapter4.labs.lab2;
 
 /**
  * Lab 2: 생성자 활용하기
- * 
+ *
  * Book 클래스를 테스트하는 메인 클래스
  */
 public class BookTest {
     public static void main(String[] args) {
         System.out.println("Book 클래스 테스트를 시작합니다.");
-        
+
         // TODO: 기본 생성자를 사용하여 Book 객체를 생성하고 정보를 출력하세요.
-        
-        
-        
+        Book book1 = new Book();
+        book1.print();
+
         // TODO: 제목만 인자로 받는 생성자를 사용하여 Book 객체를 생성하고 정보를 출력하세요.
-        
-        
-        
+        Book book2 = new Book("title");
+        book2.print();
+
         // TODO: 제목과 저자를 인자로 받는 생성자를 사용하여 Book 객체를 생성하고 정보를 출력하세요.
-        
-        
-        
+        Book book3 = new Book("title", "author");
+        book3.print();
+
         // TODO: 제목, 저자, 가격을 인자로 받는 생성자를 사용하여 Book 객체를 생성하고 정보를 출력하세요.
-        
-        
-        
+        Book book4 = new Book("title", "author", 1);
+        book4.print();
+
         // TODO: 모든 필드를 인자로 받는 생성자를 사용하여 Book 객체를 생성하고 정보를 출력하세요.
-        
-        
-        
+        Book book5 = new Book("title", "author", 1, 2025);
+        book5.print();
+
         // TODO: 객체를 생성한 후 필드 값을 변경하고 정보를 출력하세요.
-        
-        
+        Book book6 = new Book("title", "author", 2, 2025);
+        book6.setTitle("title1");
+        book6.print();
     }
-} 
+}
