018a2631502b3098fad7e07a99361b394bee3a84
namung08 <namung08@gmail.com>
2025-05-07 14:39:31 +0900

    :sparkles: feat: chapter5 lab2 AnimalTest 코드 작성

--- chapter5/labs/lab2/AnimalTest.java
+++ chapter5/labs/lab2/AnimalTest.java
@@ -1,30 +1,46 @@
 package chapter5.labs.lab2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Lab 2: 추상 클래스 활용하기
- * 
+ *
  * 여러 동물 객체를 생성하고, 각 동물의 정보와 소리를 출력하는 테스트 클래스입니다.
  */
 public class AnimalTest {
     public static void main(String[] args) {
         System.out.println("Lab 2: 추상 클래스 활용하기 실습");
-        
+
         // TODO: Dog 객체 생성
-        
-        
+        Dog dog = new Dog("corgi", true, "gogi", 1);
+
         // TODO: Cat 객체 생성
-        
-        
+        Cat cat = new Cat("korean short hair", "pig", 2);
+
         // TODO: Bird 객체 생성
-        
-        
+        Bird bird = new Bird(10, "parrot", "cherry", 3);
+
         // TODO: 각 동물의 정보와 소리 출력
-        
-        
+        dog.print();
+        dog.sound();
+
+        cat.print();
+        cat.sound();
+
+        bird.print();
+        bird.sound();
+
         // TODO: 배열을 사용하여 여러 Animal 객체 관리하기
-        
-        
+        List<Animal> animals = new ArrayList<>();
+        animals.add(cat);
+        animals.add(bird);
+        animals.add(dog);
+
         // TODO: 모든 동물에 대해 반복하여 정보와 소리 출력하기
-        
+        for (Animal animal : animals) {
+            animal.print();
+            animal.sound();
+        }
     }
-} 
+}
--- chapter5/labs/lab2/Dog.java
+++ chapter5/labs/lab2/Dog.java
@@ -2,23 +2,41 @@ package chapter5.labs.lab2;
 
 /**
  * Lab 2: 추상 클래스 활용하기
- * 
+ *
  * Dog 클래스를 정의하세요.
  * 이 클래스는 Animal 추상 클래스를 상속받아야 합니다.
  */
 public class Dog extends Animal {
-    // TODO: 강아지의 추가 속성 정의 (예: 품종)
-    
-    
-    // TODO: 생성자 정의
-    
-    
-    // TODO: 추상 메소드 sound() 구현
-    
-    
-    // TODO: 필요한 경우 부모 클래스의 메소드 오버라이딩
-    
-    
-    // TODO: 강아지만의 고유한 메소드 추가
-    
-} 
+  // TODO: 강아지의 추가 속성 정의 (예: 품종)
+  private String variety;
+  private boolean isTail;
+
+  // TODO: 생성자 정의
+  public Dog(String variety, boolean isTail, String name, int age) {
+    super(name, age);
+    this.variety = variety;
+    this.isTail = isTail;
+  }
+
+  public Dog(String variety, String name, int age) {
+    this(variety, true, name, age);
+  }
+
+  // TODO: 추상 메소드 sound() 구현
+  @Override
+  public void sound() {
+    System.out.println("Bow-wow");
+  }
+
+  // TODO: 필요한 경우 부모 클래스의 메소드 오버라이딩
+  @Override
+  public void print() {
+    super.print();
+    System.out.println("Variety: " + variety + ", isTail: " + isTail);
+  }
+
+  // TODO: 강아지만의 고유한 메소드 추가
+  public void bite() {
+    System.out.println("Bite!!");
+  }
+}
